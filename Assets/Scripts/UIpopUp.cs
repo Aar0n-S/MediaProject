76342a6ad963418c71ed4a56f51aa970248ff5f9
Aaron <Aaron2.Shanks@live.uwe.ac.uk>
2024-04-23 11:18:45 +0100

    ui pops up when close to the crate

--- Assets/Scripts/UIpopUp.cs
+++ Assets/Scripts/UIpopUp.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class UIpopUp : MonoBehaviour
 {
+
+    public GameObject ui;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        HideUI(); 
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ShowUI();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            HideUI();
+        }
+    }   
+
+
+    public void HideUI ()
+    {
+        ui.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void ShowUI ()
+    {
+        ui.SetActive(true);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
